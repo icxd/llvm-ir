cc3b9364c11c14ce52266da6be80129531f013db
icxd <icoding666@gmail.com>
2023-08-14 22:36:45 +0200

    refactored test.c to use 'array_new_with_values' instead of 'array_new' and 'array_push

--- tests/test.c
+++ tests/test.c
@@ -10,11 +10,8 @@ int main(void) {
     llvm_generator_t gen;
     llvm_init(&gen);
 
-    array(llvm_type_ptr_t) t1_members = array_new(llvm_type_ptr_t)();
-    array_push(llvm_type_ptr_t)(&t1_members, &LLVM_TYPE_INT(32));
-    array_push(llvm_type_ptr_t)(&t1_members, &LLVM_TYPE_INT(32));
-    llvm_add_type_declaration(&gen, LLVM_TYPE_DECLARATION("T1", LLVM_TYPE_STRUCTURE(t1_members, false)));
-    llvm_add_type_declaration(&gen, LLVM_TYPE_DECLARATION("T2", LLVM_TYPE_STRUCTURE(t1_members, true)));
+    llvm_add_type_declaration(&gen, LLVM_TYPE_DECLARATION("T1", LLVM_TYPE_STRUCTURE(array_new_with_values(llvm_type_ptr_t)(2, &LLVM_TYPE_INT(32), &LLVM_TYPE_INT(32)), false)));
+    llvm_add_type_declaration(&gen, LLVM_TYPE_DECLARATION("T2", LLVM_TYPE_STRUCTURE(array_new_with_values(llvm_type_ptr_t)(2, &LLVM_TYPE_INT(32), &LLVM_TYPE_INT(32)), true)));
 
     llvm_add_global(&gen, (llvm_global_t){
         .name = STR("msg"),
@@ -35,17 +32,8 @@ int main(void) {
         .alignment = 4,
     });
 
-    array(llvm_type_t) f_args = array_new(llvm_type_t)();
-    array_push(llvm_type_t)(&f_args, LLVM_TYPE_INT(32));
-    array_push(llvm_type_t)(&f_args, LLVM_TYPE_INT(32));
-    array_push(llvm_type_t)(&f_args, LLVM_TYPE_INT(32));
-
-    array(llvm_basic_block_instruction_t) f_instructions = array_new(llvm_basic_block_instruction_t)();
-    array_push(llvm_basic_block_instruction_t)(&f_instructions,
-        LLVM_BASIC_BLOCK_INSTRUCTION_INSTRUCTION(LLVM_INSTR_RETURN(LLVM_TYPE_INT(32), LLVM_VALUE_INT(0))));
-    array(llvm_basic_block_t) f_basic_blocks = array_new(llvm_basic_block_t)();
-    array_push(llvm_basic_block_t)(&f_basic_blocks, LLVM_BASIC_BLOCK("entry", f_instructions));
-    llvm_function_body_t f_body = {f_basic_blocks};
+    array(llvm_basic_block_instruction_t) f_instructions = array_new_with_values(llvm_basic_block_instruction_t)(1, LLVM_BASIC_BLOCK_INSTRUCTION_INSTRUCTION(LLVM_INSTR_RETURN(LLVM_TYPE_INT(32), LLVM_VALUE_INT(0))));
+    llvm_function_body_t f_body = {array_new_with_values(llvm_basic_block_t)(1, LLVM_BASIC_BLOCK("entry", f_instructions))};
     llvm_add_function(&gen, (llvm_function_t){
         .name = STR("f"),
         .linkage = LLVM_LINKAGE_PRIVATE,
@@ -53,43 +41,37 @@ int main(void) {
         .call_convention = LLVM_CALL_CONVENTION_COLD,
         // .dll_storage_class = LLVM_DLL_STORAGE_CLASS_DLLEXPORT,
         .return_type = LLVM_TYPE_INT(32),
-        .args = f_args,
+        .args = array_new_with_values(llvm_type_t)(3, LLVM_TYPE_INT(32), LLVM_TYPE_INT(32), LLVM_TYPE_INT(32)),
         .body = &f_body,
         .address_space = 5,
         .alignment = 4,
     });
     
-    array(llvm_type_t) print_args = array_new(llvm_type_t)();
-    array_push(llvm_type_t)(&print_args, LLVM_TYPE_STRING());
     llvm_add_function(&gen, (llvm_function_t){
         .name = STR("printf"),
         .return_type = LLVM_TYPE_INT(32),
-        .args = print_args,
+        .args = array_new_with_values(llvm_type_t)(1, LLVM_TYPE_STRING()),
         .is_vararg = true,
         .is_native = true,
     });
     
-    array(llvm_type_t) main_args = array_new(llvm_type_t)();
-    array(llvm_basic_block_t) main_basic_blocks = array_new(llvm_basic_block_t)();
     array(llvm_basic_block_instruction_t) main_instructions = array_new(llvm_basic_block_instruction_t)();
+    array(llvm_type_t) main_args = array_new(llvm_type_t)();
 
     array_push(llvm_basic_block_instruction_t)(&main_instructions,
         LLVM_BASIC_BLOCK_INSTRUCTION_LOCAL(LLVM_LOCAL(0,
             LLVM_LOCAL_INSTRUCTION(
                 LLVM_INSTR_GETELEMENTPTR("msg", LLVM_TYPE_ARRAY(LLVM_TYPE_CHAR(), 13), LLVM_VALUE_INT(0), LLVM_VALUE_INT(0))))));
-    array(llvm_function_arg_t) args = array_new(llvm_function_arg_t)();
-    array_push(llvm_function_arg_t)(&args, (llvm_function_arg_t){
-        LLVM_TYPE_STRING(),
-        LLVM_VALUE_LOCAL(0),
-    });
+    
+    array(llvm_function_arg_t) args = array_new_with_values(llvm_function_arg_t)(1, (llvm_function_arg_t){ LLVM_TYPE_STRING(), LLVM_VALUE_LOCAL(0), });
+
     array_push(llvm_basic_block_instruction_t)(&main_instructions,
         LLVM_BASIC_BLOCK_INSTRUCTION_LOCAL(LLVM_LOCAL(1, LLVM_LOCAL_INSTRUCTION(
             LLVM_INSTR_CALL(LLVM_TYPE_INT(32), "printf", args)))));
     array_push(llvm_basic_block_instruction_t)(&main_instructions,
         LLVM_BASIC_BLOCK_INSTRUCTION_INSTRUCTION(LLVM_INSTR_RETURN(LLVM_TYPE_INT(32), LLVM_VALUE_INT(0))));
-    array_push(llvm_basic_block_t)(&main_basic_blocks, LLVM_BASIC_BLOCK("entry", main_instructions));
     
-    llvm_function_body_t main_body = {main_basic_blocks};
+    llvm_function_body_t main_body = {array_new_with_values(llvm_basic_block_t)(1, LLVM_BASIC_BLOCK("entry", main_instructions))};
     llvm_add_function(&gen, (llvm_function_t){
         .name = STR("main"),
         .return_type = LLVM_TYPE_INT(32),
